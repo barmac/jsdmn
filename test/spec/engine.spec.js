ba020463251d9119c5c21cfc408b8a306b1842e1
Maciej Barelkowski <maciejbarel@gmail.com>
2025-01-16 20:08:45 +0100

    test: verify context is passed

--- test/spec/engine.spec.js
+++ test/spec/engine.spec.js
@@ -16,4 +16,19 @@ describe('evaluator', function() {
     // then
     expect(result).to.eql(4);
   });
+
+
+  it('should evaluate a decision with input', async function() {
+
+    // given
+    const engine = new Engine();
+    const dmn = getFixture('input.dmn');
+    await engine.load(dmn);
+
+    // when
+    const result = await engine.evaluateDecision('Decision', { a: 2, b: 2 });
+
+    // then
+    expect(result).to.eql(4);
+  });
 });
